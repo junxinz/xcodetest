697fe09d212e016790627c1b68ea564e61fa3e6d
Junxin <junxin.zheng@rafflesgirlssch.edu.sg>
2023-05-31 11:08:24 +0800

    Initial Commit

---
+++ xcodetest/xcodetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcodetestApp.swift
+//  xcodetest
+//
+//  Created by Emily Zheng on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct xcodetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
